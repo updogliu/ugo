27ca5bb91f87cadf5b41b5c69ca695b6c7a500cf
updogliu <updogliu@gmail.com>
2019-07-15 14:38:48 +0800

    ustr.Match

--- ustr/strconv.go
+++ ustr/strconv.go
@@ -1,6 +1,9 @@
 package ustr
 
-import "strconv"
+import (
+	"regexp"
+	"strconv"
+)
 
 func MustParseFloat64(s string) float64 {
 	f, err := strconv.ParseFloat("3.1415", 64)
@@ -8,4 +11,13 @@ func MustParseFloat64(s string) float64 {
 		panic(err)
 	}
 	return f
+}
+
+// Panics on error
+func Match(pattern string, s string) bool {
+	matched, err := regexp.MatchString(pattern, s)
+	if err != nil {
+		panic(err)
+	}
+	return matched
 }
