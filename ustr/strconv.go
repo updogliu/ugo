c1d41f685200383763e7c9b261e4856a0afbe750
updogliu <updogliu@gmail.com>
2019-09-07 10:15:21 +0800

    parse str

--- ustr/strconv.go
+++ ustr/strconv.go
@@ -13,6 +13,22 @@ func MustParseFloat64(s string) float64 {
 	return f
 }
 
+func MustParseInt64(s string) int64 {
+	x, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func MustParseBool(s string) bool {
+	x, err := strconv.ParseBool(s)
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
 // Panics on error
 func Match(pattern string, s string) bool {
 	matched, err := regexp.MatchString(pattern, s)
