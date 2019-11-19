c51d76487a7d7c708770507a6effaf78d4713987
updogliu <updogliu@gmail.com>
2019-11-19 11:10:56 +0800

    inplace append

---
+++ ugo.go
@@ -0,0 +1,33 @@
+package ugo
+
+func AppendStr(a *[]string, val string) {
+	*a = append(*a, val)
+}
+
+func AppendInt(a *[]int, val int) {
+	*a = append(*a, val)
+}
+
+func AppendI64(a *[]int64, val int64) {
+	*a = append(*a, val)
+}
+
+func AppendUint(a *[]uint, val uint) {
+	*a = append(*a, val)
+}
+
+func AppendU8(a *[]uint8, val uint8) {
+	*a = append(*a, val)
+}
+
+func AppendU64(a *[]uint64, val uint64) {
+	*a = append(*a, val)
+}
+
+func AppendByte(a *[]byte, val byte) {
+	*a = append(*a, val)
+}
+
+func AppendF64(a *[]float64, val float64) {
+	*a = append(*a, val)
+}
