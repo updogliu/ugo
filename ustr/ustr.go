fce358f9644a96f3567a0cae89fae4783420d966
updogliu <updogliu@gmail.com>
2019-11-20 14:49:08 +0800

    lowerhas

--- ustr/ustr.go
+++ ustr/ustr.go
@@ -5,3 +5,7 @@ import "strings"
 func SameLower(a, b string) bool {
 	return strings.ToLower(a) == strings.ToLower(b)
 }
+
+func LowerHas(a, sub string) bool {
+	return strings.Contains(strings.ToLower(a), strings.ToLower(sub))
+}
