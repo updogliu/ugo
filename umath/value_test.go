81759695708635bbd72bdedd06f5f340a5f40d4f
updogliu <updogliu@gmail.com>
2019-12-31 16:04:50 +0800

    add a test

--- umath/value_test.go
+++ umath/value_test.go
@@ -25,6 +25,7 @@ func TestI64Round(t *testing.T) {
 }
 
 func TestStdPercentile(t *testing.T) {
+	r.Equal(t, float64(0), StdPercentile(100, 100, 20))
 	r.InEpsilon(t, 0.682689492137086, StdPercentile(120, 100, 20), 1e-8)
 	r.InEpsilon(t, 0.954499736103642, StdPercentile(-10, 0, 5), 1e-8)
 	r.InEpsilon(t, 0.997300203936740, StdPercentile(5, -10, 5), 1e-8)
