6e3d38c388f751041865b919d4c1b40c12ded0a4
updogliu <updogliu@gmail.com>
2020-03-30 20:26:33 +0800

    change API of Retry

--- utask/retry.go
+++ utask/retry.go
@@ -11,11 +11,11 @@ import (
 // Returns nil on the first time `f()` returns nil, even if by that time `ctx` has
 // been cancelled. Otherwise returns the last error returned by `f()`. If `f()` has
 // never got a chance to run, returns `ctx.Err()`.
-func Retry(ctx context.Context, taskName string, retryGap time.Duration, f func() error) error {
+func Retry(ctx context.Context, taskName string, retryGapMs int64, f func() error) error {
 	// Suppress logging more than one error within 5 sec.
 	logCooldown := utime.NewUnreadyCooldown(5 * time.Second)
 
-	ticker := time.NewTicker(retryGap)
+	ticker := time.NewTicker(retryGapMs * time.Millisecond)
 	defer ticker.Stop()
 
 	var lastErr error
